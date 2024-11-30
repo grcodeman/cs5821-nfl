78eb7bc277036a0e436690066779678b554b6f21
grcodeman <grcodeman@gmail.com>
2024-11-30 10:20:01 -0500

    main.py inputs

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+
+
 def main():
-    return 0
+
+    team_away_input = input("\nAway Team: ")
+    team_home_input = input("\nHome Team: ")
+    game_ou_input = input("\nO/U: ")
+
+    return 0    
 
 if __name__ == '__main__':
     main()
