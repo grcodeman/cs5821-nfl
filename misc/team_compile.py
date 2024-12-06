bef98e2b1ccd0e7a06742cd89e4fac800caea05b
grcodeman <grcodeman@gmail.com>
2024-12-05 19:06:20 -0500

    add opp_ statistics in stat compiler

--- misc/team_compile.py
+++ misc/team_compile.py
@@ -15,6 +15,18 @@ home_df.columns = home_df.columns.str.replace('_home', '')
 home_df.rename(columns={'home': 'team'}, inplace=True)
 home_df['home_or_away'] = 'home'
 
+# add opp stats from home for away teams
+for col in df.columns:
+    if col.endswith('_home'):
+        new_col = 'opp_' + col.replace('_home', '')
+        away_df[new_col] = df[col]
+
+# add opp stats from away for home teams
+for col in df.columns:
+    if col.endswith('_away'):
+        new_col = 'opp_' + col.replace('_away', '')
+        home_df[new_col] = df[col]
+
 # merge df
 team_stats = pd.concat([away_df, home_df], ignore_index=True)
 
@@ -24,6 +36,16 @@ team_stats[numeric_cols] = team_stats[numeric_cols].apply(pd.to_numeric, errors=
 
 # average
 team_season_avg = team_stats.groupby(['season', 'team'])[numeric_cols].mean().reset_index()
+
+# get current cols
+cols = team_season_avg.columns.tolist()
+
+# loop through cols to move opp_ prefix stats to the back
+for col in cols[:]:
+    if col.startswith('opp_'):
+        cols.remove(col)
+        cols.append(col)
+team_season_avg = team_season_avg[cols]
 
 # save to csv
 team_season_avg.to_csv('data/nfl_team_stats.csv', index=False)
